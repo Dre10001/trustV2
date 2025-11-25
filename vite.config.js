46d6450b57ff644d32bddb2084eba296806de390
Dre10001 <140208800+Dre10001@users.noreply.github.com>
2025-11-25 20:54:12 +0000

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // MUST match the GitHub Pages URL path (note the lowercase t)
+  base: "/trustV2/",
+  build: {
+    outDir: "docs",   // Vite will put the built site here
+  },
 });
